d8b4868aef987a742289130d396ceb7e9a696a15
alextisch <gonc.jm88@gmail.com>
2025-06-02 18:16:47 +0000

    Formatted with clang-format (Mozilla style)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello, world from " << name << "!";
-    return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << "!";
+  return 0;
 }
